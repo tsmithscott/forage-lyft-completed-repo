df6bce7cf6553d7de5efd1672f68bfb2aa8419d8
Theodore Smith Scott <theosmithscott@protonmail.com>
2023-01-30 23:35:56 +0000

    Added wrappers to static methods

--- car/car_factory.py
+++ car/car_factory.py
@@ -11,30 +11,31 @@ from car import Car
 
 class CarFactory:
     
+    @staticmethod
     def create_calliope(self, current_date: date, last_service_date: date,
                         current_mileage: int, last_service_mileage: int):
         return Car(CapuletEngine(last_service_mileage, current_mileage),
                    SpindlerBattery(last_service_date, current_date))
         
-
+    @staticmethod
     def create_glissade(current_date: date, last_service_date: date,
                         current_mileage: int, last_service_mileage: int):
         return Car(WilloughbyEngine(last_service_mileage, current_mileage),
                    SpindlerBattery(last_service_date, current_date))
 
-
+    @staticmethod
     def create_palindrome(current_date: date, last_service_date: date,
                           warning_light_on: bool):
         return Car(SternmanEngine(warning_light_on),
                    SpindlerBattery(last_service_date, current_date))
         
-    
+    @staticmethod
     def create_rorschach(current_date: date, last_service_date: date,
                          current_mileage: int, last_service_mileage: int):
         return Car(WilloughbyEngine(last_service_mileage, current_mileage),
                    NubbinBattery(last_service_date, current_date))
         
-    
+    @staticmethod
     def create_thovex(current_date: date, last_service_date: date,
                       current_mileage: int, last_service_mileage: int):
         return Car(CapuletEngine(last_service_mileage, current_mileage),
